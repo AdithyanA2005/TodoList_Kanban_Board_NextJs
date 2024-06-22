3a0106b9f4a4c678b36082499605e23818fca1d4
Adithyan A <deepaadithyan56@gmail.com>
2024-06-22 15:26:12 +0530

    add environment variables example file. add type intellisense for env variables

---
+++ src/lib/types.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_AW_ENDPOINT?: string;
+    NEXT_PUBLIC_AW_PROJECT_ID?: string;
+    NEXT_PUBLIC_AW_DATABASE_ID?: string;
+    NEXT_PUBLIC_AW_TODOS_COLLECTION_ID?: string;
+  }
+}
