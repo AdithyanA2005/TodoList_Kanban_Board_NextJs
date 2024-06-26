53717b86162732485196f4a2015b47a824a33abf
Adithyan A <deepaadithyan56@gmail.com>
2024-06-26 10:51:47 +0530

    move environment types to a separate environment.ts file instead of d.ts file

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -22,11 +22,3 @@ interface IColumn {
 
 type IColumns = Map<ETaskTypes, IColumn>;
 
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NEXT_PUBLIC_AW_ENDPOINT?: string;
-    NEXT_PUBLIC_AW_PROJECT_ID?: string;
-    NEXT_PUBLIC_AW_DATABASE_ID?: string;
-    NEXT_PUBLIC_AW_TODOS_COLLECTION_ID?: string;
-  }
-}
