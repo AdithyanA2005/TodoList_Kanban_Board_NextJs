f4cd42de402718094259568d3423b9372ac3f95b
Adithyan A <deepaadithyan56@gmail.com>
2024-06-27 11:08:15 +0530

    make add new button in columns only visible on column hover

--- src/components/column.tsx
+++ src/components/column.tsx
@@ -41,7 +41,7 @@ export default function Column({ id, todos, index }: ColumnProps) {
                 ref={provided.innerRef}
                 className={`p-2 rounded-2xl shadow-sm ${snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"}`}
               >
-                <h2 className="flex justify-between font-bold text-xl p-1.5">
+                <h2 className="group h-10 flex justify-between font-bold text-xl p-1.5">
                   <div className="flex items-center gap-1">
                     <span>{getColumnDisplayName(id)}</span>
                     <span className="h-7 aspect-square text-gray-500 bg-gray-200 rounded-full px-2 py-1 font-mono text-xs flex justify-center items-center">
@@ -53,9 +53,9 @@ export default function Column({ id, todos, index }: ColumnProps) {
 
                   <button
                     onClick={handleAddTodo}
-                    className="rounded-full bg-gray-200/50 hover:bg-green-200/80 filter backdrop-blur-3xl p-[5px] text-gray-700 hover:text-green-800"
+                    className="hidden group-hover:block rounded-full bg-gray-200/50 hover:bg-green-200/80 filter backdrop-blur-3xl p-[4px] text-gray-700 hover:text-green-800"
                   >
-                    <PlusIcon className="size-[22px]" />
+                    <PlusIcon className="size-[21px]" />
                   </button>
                 </h2>
 
